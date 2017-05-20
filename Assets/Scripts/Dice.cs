6229570f34cda63d0c386129ac5e5e792165d50e
Alexander Xu <ajm.xu@outlook.com>
2017-05-20 20:52:18 +0100

    Initial Commit

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour {
+
+	public Text score;
+	public Text highScore;
+
+	void Start() {
+		highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+	}
+
+	public void RollDice() {
+		int num = Random.Range(1, 7);
+		score.text = num.ToString();
+
+		if (num > PlayerPrefs.GetInt("HighScore", 0)) {
+			highScore.text = num.ToString();
+			PlayerPrefs.SetInt("HighScore", num);
+		}
+	}
+
+	public void Reset() {
+		highScore.text = "0";
+		PlayerPrefs.SetInt("HighScore", 0);
+		// or PlayerPrefs.DeleteAll();
+	}
+}
